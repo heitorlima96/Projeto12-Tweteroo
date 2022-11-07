02971730e11169114327f54b523c303a891d7d3a
heitorlima96 <limaheitor.1996@gmail.com>
2022-11-07 12:09:26 -0300

    post and get - tweets

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(json());
 app.use(cors());
 
 const user = []
+const tweets = []
 
 app.post('/sign-up', (req, res) => {
     const { username, avatar } = req.body
@@ -16,6 +17,30 @@ app.post('/sign-up', (req, res) => {
 
     res.send('OK');
     
+});
+
+app.get('/tweets', (req, res) => {
+    res.send(tweets.slice(tweets.length - 10, tweets.length));
+});
+
+app.post('/tweets', (req, res) => {
+    const { username, tweet } = req.body
+    const usuario = user.find(user => {
+        if (user.username === username) return true
+    })
+
+    const { avatar } = usuario
+
+    tweets.push(
+        {
+            username,
+            avatar,
+            tweet
+        }
+    )
+
+    res.send('OK');
+   
 });
 
 
